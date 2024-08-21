17b11d9dfdc6eaa2893dfbf311e8eca704d2d10e
CodingSwordfish <wagobryan@gmail.com>
2024-08-21 21:49:07 +0300

    Implement delete button and AC button

--- calc.js
+++ calc.js
@@ -1,67 +1,76 @@
-let currentValue="";
-let previousValue="";
-let operator = "";
-
 // Get references to the main elements
-const calculator = document.getElementById('calculator');
-const largeScreen = document.getElementById('largeScreen');
-const prevScreen = document.getElementById('prevScreen');
-const currentScreen = document.getElementById('currentScreen');
+const calculator = document.getElementById("calculator");
+const largeScreen = document.getElementById("largeScreen");
+const prevScreen = document.getElementById("prevScreen");
+const currentScreen = document.getElementById("currentScreen");
 
 // Get references to the number buttons
-const numberButtons = document.querySelectorAll('#numbers');
+const numberButtons = document.querySelectorAll(".numbers");
 
 // Get references to the operator buttons
-const operatorButtons = document.querySelectorAll('#operator');
+const operatorButtons = document.querySelectorAll(".operator");
 
 // Get references to other buttons
-const decimalButton = document.getElementById('decimal');
-const equalsButton = document.getElementById('equals');
+const decimalButton = document.getElementById("decimal");
+const equalsButton = document.getElementById("equals");
 
+// Get reference to the AC button
+const clearAll = document.querySelector("#ac");
 
-numberButtons.forEach((button)=>{
-button.addEventListener("click",(e)=>{
-  console.log("hii")
+//Get reference to the delete button
+const del = document.querySelector("#del");
+
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    handleNumbers(e.target.textContent);
+    currentScreen.textContent += button.textContent;
   });
-})
+});
 
-operatorButtons.forEach((button)=>{
-  button.addEventListener("click",(e)=>{
-    console.log("Helloo")
-  })
-})
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    handleOp(e.target.textContent);
+    prevScreen.textContent = previousValue + "" + operator;
+    currentScreen.textContent = currentValue;
+    console.log(e);
+  });
+});
 
-equalsButton.addEventListener("click",(e)=>{
-  console.log("equals")
-})
+equalsButton.addEventListener("click", (e) => {
+  console.log("equals");
+});
 
-decimalButton.addEventListener("click",(e)=>{
-  console.log("decimal")
-})
+decimalButton.addEventListener("click", (e) => {
+  console.log("decimal");
+});
 
+clearAll.addEventListener("click", (e) => {
+  currentValue = "";
+  previousValue = "";
+  currentScreen.textContent = "";
+  prevScreen.textContent = "";
+  operator = "";
+});
 
+del.addEventListener("click", (e) => {
+  currentValue = currentValue.slice(0, -1);
+  currentScreen.textContent = currentValue;
+});
 
-function handleNumbers(){
-
+function handleNumbers(num) {
+  currentValue += num;
 }
 
-function handleOp(){
-
+function handleOp(op) {
+  operator = op;
+  previousValue = currentValue;
+  currentValue = "";
 }
 
-function clear(){
+function calculate() {}
 
-}
-  
-function calculate (){
-
-}
-
-function handleDecimal(){
-
-}
-  
-
-
-
- 
+function handleDecimal() {}
