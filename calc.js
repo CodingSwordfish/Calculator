6bbb58f020a07e709b412056622b3dc5085b23f6
CodingSwordfish <wagobryan@gmail.com>
2024-08-23 12:12:33 +0300

    Add logic to handle number

--- calc.js
+++ calc.js
@@ -9,3 +9,17 @@ let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operator");
 let previousScreen = document.querySelector(".previous");
 let currentScreen = document.querySelector(".current");
+
+ numbers.forEach((number)=>{
+number.addEventListener("click",(e)=>{
+    handleNumber(e.target.textContent);
+    currentScreen.textContent= currentValue;
+})
+ })
+
+ function handleNumber(num){
+    if(currentValue.length <5){
+        currentValue+= num;
+    }
+
+ }
