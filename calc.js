74105e4418b1f45776483481ec53c4c5ef544651
CodingSwordfish <wagobryan@gmail.com>
2024-08-23 11:48:31 +0300

    Reference the HTML DOM nodes and store them in a variable

---
+++ calc.js
@@ -0,0 +1,11 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+let clear =document.querySelector("#clear");
+let equals = document.querySelector("#equals");
+let decimal = document.querySelector("#decimal");
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operator");
+let previousScreen = document.querySelector(".previous");
+let currentScreen = document.querySelector(".current");
