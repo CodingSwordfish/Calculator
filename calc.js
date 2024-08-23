dd099764b09f30c896e776e75d9eb1fb2d4612b2
CodingSwordfish <wagobryan@gmail.com>
2024-08-23 12:30:49 +0300

    Implement the split screen function and the clear button

--- calc.js
+++ calc.js
@@ -10,6 +10,14 @@ let operators = document.querySelectorAll(".operator");
 let previousScreen = document.querySelector(".previous");
 let currentScreen = document.querySelector(".current");
 
+
+clear.addEventListener("click",(e)=>{
+previousValue = "";
+currentValue="";
+previousScreen.textContent="";
+currentScreen.textContent="";
+});
+
  numbers.forEach((number)=>{
 number.addEventListener("click",(e)=>{
     handleNumber(e.target.textContent);
@@ -17,9 +25,24 @@ number.addEventListener("click",(e)=>{
 })
  })
 
+ operators.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+handleOperator(e.target.textContent)
+previousScreen.textContent = previousValue + " "+ operator;
+currentScreen.textContent = currentValue;
+    })
+ })
+
  function handleNumber(num){
     if(currentValue.length <5){
         currentValue+= num;
     }
+
+ }
+
+ function handleOperator(op){
+    operator = op;
+    previousValue = currentValue;
+    currentValue= ""
 
  }
